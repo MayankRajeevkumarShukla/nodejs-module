04b5fa6b0d18de77fdcde618ddec62964fe96b5d
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2023-03-18 19:38:42 +0000

    uadd index .js to add numbers

---
+++ index.js
@@ -0,0 +1,9 @@
+// let num1 = 23;
+// let num2 = 45;
+// let result = num1 + num2;
+// console.log(`sum of numbers ${num1} and ${num2} is ${result} `);
+// ${} place holders it prints the value of variable which is given in curly bracket.
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let result = num1 + num2;
+console.log(`sum of numbers ${num1} and ${num2} is ${result} `);
