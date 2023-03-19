2aa917267b7305f8d902dd7a94035b77e999b6c0
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2023-03-19 18:23:30 +0000

    add sever.js

---
+++ scratch.js
@@ -0,0 +1,18 @@
+// const fs = require("fs");
+// const data = fs.readFileSync("README.md", "utf-8");
+// const data = fs.readFileSync("index.js", "utf-8");
+// console.log(data);
+// const path = require("path")
+// // "my folder/utils/index.js"
+//  let newPath = path.join("my-folder","utils", "index.js")
+//  console.log(newPath);
+// let numbers =[2,3,4,5,6];
+// let fruits = ["apple","banana","orange"]
+// // numbers.forEach(function(number){
+// //     //forEach helps to call the function one by one
+// //     console.log(number)
+// // })
+// fruits.forEach(function(number, index){
+//     //forEach helps to call the function one by one
+//     console.log(number,index)
+// })
